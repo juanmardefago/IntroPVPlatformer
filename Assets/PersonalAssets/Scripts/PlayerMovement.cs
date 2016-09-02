99ac7416c4651f3e35d8237fd2235e23e7fa3d43
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2016-09-02 04:02:54 -0300

    Agregadas animaciones basicas y revampeado el PlayerMovement

--- Assets/PersonalAssets/Scripts/PlayerMovement.cs
+++ Assets/PersonalAssets/Scripts/PlayerMovement.cs
@@ -15,6 +15,8 @@ public class PlayerMovement : MonoBehaviour {
     public float maxTimeJumping;
     private bool letGoJump;
     private bool isCurrentlyJumping;
+    private Animator anim;
+    private bool facingRight = true;
 
 
 	// Use this for initialization
@@ -22,6 +24,7 @@ public class PlayerMovement : MonoBehaviour {
         rBody = GetComponent<Rigidbody2D>();
         jumpTimer = 0f;
         letGoJump = false;
+        anim = GetComponent<Animator>();
 	}
 	
 	// Hago el check de si un player quiere saltar en update porque en general se va a llamar más veces por segundo que el fixedUpdate.
@@ -36,8 +39,7 @@ public class PlayerMovement : MonoBehaviour {
 
     // En el fixed update hago los cambios basicos de fisica, si se esta moviendo horizontalmente y el checkeo de jump.
     void FixedUpdate () {
-        moveX = Input.GetAxis("Horizontal");
-        rBody.velocity = new Vector2(moveX * speed, rBody.velocity.y);
+        CheckHorizontalMovement();
         CheckJump();
     }
 
@@ -50,7 +52,39 @@ public class PlayerMovement : MonoBehaviour {
             this.jumpTimer = 0f;
             letGoJump = false;
             isCurrentlyJumping = false;
+            anim.SetBool("isJumping", false);
         }
+    }
+
+    private void CheckHorizontalMovement()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        if (moveX != 0) {
+            rBody.velocity = new Vector2(moveX * speed, rBody.velocity.y);
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("speed", Mathf.Abs(moveX));
+        } else {
+            anim.SetBool("isWalking", false);
+            anim.SetFloat("speed", 0);
+        }
+
+        CorrectLocalScale(moveX);
+    }
+
+    private void CorrectLocalScale(float axis)
+    {
+        if((axis > 0 && !facingRight) || (axis < 0 && facingRight))
+        {
+            FlipScale();
+        }
+    }
+
+    private void FlipScale()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x = scale.x * -1;
+        transform.localScale = scale;
     }
 
     //hace el checkeo de Jump
@@ -63,6 +97,7 @@ public class PlayerMovement : MonoBehaviour {
             jumpTimer += Time.deltaTime;
             wantsToJump = false;
             isCurrentlyJumping = true;
+            anim.SetBool("isJumping", true);
         } // Si sigue queriendo saltar, pero además esta saltando, se checkea si no se le agoto el timer de salto y si no solto la tecla.
         else if (wantsToJump && isCurrentlyJumping && jumpTimer < maxTimeJumping && !letGoJump){
             jumpTimer += Time.deltaTime;

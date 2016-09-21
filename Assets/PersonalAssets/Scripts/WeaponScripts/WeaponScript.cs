fda51f7b82dfbfeb78ff92d8b3d7b3700f14e562
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2016-09-20 23:02:30 -0300

    Agregado playerWeapon prefab y cambiado el weaponscript
    Ahora usando como base ese prefab y las variables del weapon script se
    pueden armar diferentes armas con diferentes proyectiles

--- Assets/PersonalAssets/Scripts/WeaponScripts/WeaponScript.cs
+++ Assets/PersonalAssets/Scripts/WeaponScripts/WeaponScript.cs
@@ -6,27 +6,32 @@ public class WeaponScript : MonoBehaviour {
     public Rigidbody2D laserShot;
     public Rigidbody2D laserChargedShot;
 
-    private float chargedShotCD = 1.5f;
+
+    public float chargedShotCD = 1.5f;
     private float chargedShotTimer = 0f;
 
-	// Use this for initialization
-	void Start () {
+    public float normalShotCD = 0.2f;
+    private float normalShotTimer = 0f;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (chargedShotTimer >= Time.deltaTime) {
-            chargedShotTimer -= Time.deltaTime;
-        } else if (ChargedShotOnCD()) {
-            chargedShotTimer = 0f;
-        }
-	}
+        UpdateNormalShotTimer();
+        UpdateChargedShotTimer();
+    }
 
     public void Fire(int localScaleFlipFactor) {
-        Rigidbody2D shot = (Rigidbody2D) Instantiate(laserShot);
-        shot.transform.position = transform.position + new Vector3(0.001f * localScaleFlipFactor, 0, 0);
-        shot.velocity = new Vector2(15 * localScaleFlipFactor, 0);
+        if (!NormalShotOnCD())
+        {
+            Rigidbody2D shot = (Rigidbody2D)Instantiate(laserShot);
+            shot.transform.position = transform.position + new Vector3(0.001f * localScaleFlipFactor, 0, 0);
+            shot.velocity = new Vector2(15 * localScaleFlipFactor, 0);
+            normalShotTimer = normalShotCD;
+        }
     }
 
     public void ChargedFire(int localScaleFlipFactor)
@@ -40,8 +45,37 @@ public class WeaponScript : MonoBehaviour {
         }
     }
 
+    private void UpdateChargedShotTimer()
+    {
+        if (chargedShotTimer >= Time.deltaTime)
+        {
+            chargedShotTimer -= Time.deltaTime;
+        }
+        else if (ChargedShotOnCD())
+        {
+            chargedShotTimer = 0f;
+        }
+    }
+
+    private void UpdateNormalShotTimer()
+    {
+        if (normalShotTimer >= Time.deltaTime)
+        {
+            normalShotTimer -= Time.deltaTime;
+        }
+        else if (NormalShotOnCD())
+        {
+            normalShotTimer = 0f;
+        }
+    }
+
     private bool ChargedShotOnCD()
     {
         return chargedShotTimer != 0f;
+    }
+
+    private bool NormalShotOnCD()
+    {
+        return normalShotTimer != 0f;
     }
 }

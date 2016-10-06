5a4eec9424a48940bfab31d5296701e89db366aa
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2016-10-06 06:44:07 -0300

    Agregadas un par de fuentes, popup de daño y revamp de folders de prefab

---
+++ Assets/PersonalAssets/Scripts/UIScripts/PopupText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopupText : MonoBehaviour {
+
+    public Animator anim;
+
+    private Text textToDisplay;
+
+	// Use this for initialization
+	void Start () {
+        AnimatorClipInfo[] clipInfo = anim.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+        textToDisplay = anim.GetComponent<Text>();
+	}
+
+    public void SetText(string text)
+    {
+        textToDisplay.text = text;
+    }
+	
+}
---
+++ Assets/PersonalAssets/Scripts/UIScripts/PopupTextHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopupTextHandler : MonoBehaviour
+{
+
+    public GameObject popupPrefab;
+    public Animator prefabAnim;
+    private GameObject canvas;
+    private float clipLength;
+
+    public void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        GameObject popup = Instantiate(popupPrefab);
+        if (clipLength == 0f)
+        {
+            AnimatorClipInfo[] clipInfo = popup.GetComponentInChildren<Animator>().GetCurrentAnimatorClipInfo(0);
+            clipLength = clipInfo[0].clip.length;
+        }
+        popup.GetComponentInChildren<Text>().text = damage.ToString();
+        popup.transform.SetParent(canvas.transform, false);
+        Vector2 screenLocation = Camera.main.WorldToScreenPoint(new Vector2(transform.position.x + Random.Range(-0.2f, 0.2f), transform.position.y + Random.Range(-0.2f, 0.2f)));
+        popup.transform.position = screenLocation;
+        Destroy(popup, clipLength);
+    }
+}

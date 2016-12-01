36a761401a0330a2563249232e6eb211d46ff0b2
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2016-12-01 01:41:56 -0300

    BGM Changer armado y BGMs puestas en su lugar!

---
+++ Assets/PersonalAssets/Scripts/BGMChangerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMChangerScript : MonoBehaviour
+{
+    public AudioClip areaMusic;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = Camera.main.GetComponent<AudioSource>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        audioSource.clip = areaMusic;
+        audioSource.Play();
+    }
+}
